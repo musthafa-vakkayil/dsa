8ea3d8fee91e82d65caba92ba5e397ab248ddd94
Muhammed Musthafa Shahal V <t-m.shahal.v@heart.org>
2025-02-10 22:17:23 +0530

    feat: two sum and duplicate problems

---
+++ 03 Arrays/duplicates.go
@@ -0,0 +1,13 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+func containsDuplicate(nums []int) bool {
+    sort.Ints(nums)
+
+    for i := 0; i < len(nums) - 1; i++ {
+        if nums[i] == nums[i + 1] {
+            return true
+        }
+    }
+
+    return false
+}
